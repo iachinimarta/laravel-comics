3655ebf87cd507e39bba9cf46a7cdb179f399081
Marta Iachini <iachinimarta@gmail.com>
2022-09-28 16:38:22 +0200

    Modifiche: -inserito footer -inserito jumbotron -impostazione sezione cards

--- resources/views/links/comics.blade.php
+++ resources/views/links/comics.blade.php
@@ -1,12 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+@extends('structure.structure')
+
+@section('main_content')
+    <div>
+        <div class="card-container">
+            <div class="card" >
+                <a href="#">
+                    <img src="" alt="">
+                    <div>prova</div>
+                </a>
+            </div>
+        </div>
+    </div> 
+@endsection
